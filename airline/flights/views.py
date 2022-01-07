ea57eeb2ea677c2f5c7b0f017d292527c9399e5d
Maks1789 <95221275+Maks1789@users.noreply.github.com>
2022-01-07 12:06:30 +0200

    Add book

--- airline/flights/views.py
+++ airline/flights/views.py
@@ -1,11 +1,29 @@
 from django.shortcuts import render
-from . models import Flight
+from . models import Flight, Passenger, Airport
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 # Create your views here.
 
 def index(request):
     return render(request, "flights/index.html", {
         "flights": Flight.objects.all()
     })
-def flight (request, flight_id):
-    flight = Flight.objects.get(pk = flight_id)
-    return render(request,  "flights/flight.html", {"flight": flight, "passengers": flight.passengers.all()})
+
+
+def flight(request, flight_id):
+    flight = Flight.objects.get(id=flight_id)
+    passengers = flight.passengers.all()
+    non_passengers = Passenger.objects.exclude(flights=flight).all()
+    return render(request, "flights/flight.html", {
+        "flight": flight,
+        "passengers": passengers,
+        "non_passengers": non_passengers
+    })
+
+def book(request, flight_id):
+    if request.method == "POST":       # Якщо запит post - додаємо новий рейс
+        flight = Flight.objects.get(pk=flight_id)  # Отримуємо доступ до рейсів
+        passenger_id = int(request.POST["passenger"])# Знаходимо id пасажира через дані з надісланої форми
+        passenger = Passenger.objects.get(pk=passenger_id) # Знаходимо пасажира за id
+        passenger.flights.add(flight)# Додаємо пасажира на рейс
+        return HttpResponseRedirect(reverse("flight", args=(flight.id,)))# Перенаправляємо користувача на сторінку рейсів
