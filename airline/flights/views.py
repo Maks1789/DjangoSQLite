f647175e16519a9cf76cdd82beded88676ca6ebc
Maks1789 <95221275+Maks1789@users.noreply.github.com>
2022-01-08 10:23:55 +0200

    user auth, admin mod

--- airline/flights/views.py
+++ airline/flights/views.py
@@ -4,6 +4,7 @@ from django.http import HttpResponseRedirect
 from django.urls import reverse
 # Create your views here.
 
+
 def index(request):
     return render(request, "flights/index.html", {
         "flights": Flight.objects.all()
@@ -21,9 +22,9 @@ def flight(request, flight_id):
     })
 
 def book(request, flight_id):
-    if request.method == "POST":       # Якщо запит post - додаємо новий рейс
-        flight = Flight.objects.get(pk=flight_id)  # Отримуємо доступ до рейсів
-        passenger_id = int(request.POST["passenger"])# Знаходимо id пасажира через дані з надісланої форми
-        passenger = Passenger.objects.get(pk=passenger_id) # Знаходимо пасажира за id
-        passenger.flights.add(flight)# Додаємо пасажира на рейс
+    if request.method == "POST":                                         # Якщо запит post - додаємо новий рейс
+        flight = Flight.objects.get(pk=flight_id)                        # Отримуємо доступ до рейсів
+        passenger_id = int(request.POST["passenger"])                    # Знаходимо id пасажира через дані з надісланої форми
+        passenger = Passenger.objects.get(pk=passenger_id)               # Знаходимо пасажира за id
+        passenger.flights.add(flight)                                    # Додаємо пасажира на рейс
         return HttpResponseRedirect(reverse("flight", args=(flight.id,)))# Перенаправляємо користувача на сторінку рейсів
